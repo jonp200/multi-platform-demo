c3df847473221f3d6882a7cffe07563e8b6f0246
Steven Ward <29103029+Stedoss@users.noreply.github.com>
2024-04-18 13:18:13 +0100

    Update to correctly conditionally compile

--- handler/evokeos.go
+++ handler/evokeos.go
@@ -1,15 +1,12 @@
-package evokeHandler
+//go:build evokeos
+
+package handler
 
 import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"multi-platform-demo/os/configuration"
 )
-
-type Handler struct {
-	Config configuration.Configuration
-}
 
 func (h Handler) GetOSName(c echo.Context) error {
 	return c.String(http.StatusOK, "evokeos, config path: "+h.Config.Path)
